68d143d86171ef28f8ff146844d1f9359437421c
zhanglioncel <zhanglioncel@gmail.com>
2022-09-24 13:56:42 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-
+import pandas
 import streamlit
 streamlit.title("My Parents New Healthy Dinner")
 streamlit.header("🍞Breakfast Menu")
@@ -8,3 +8,5 @@ streamlit.text("🐔Rocket Smoothie")
 streamlit.text("🥑Hard-Boiled Free-Range Egg")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
