052b18530ebb947b97d195da754f300b7445e335
zhanglioncel <zhanglioncel@gmail.com>
2022-09-24 13:48:05 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 
 import streamlit
 streamlit.title("My Parents New Healthy Dinner")
+streamlit.header("reakfast Menu")
+streamlit.text("Omega3 & Blueberry Oatmeal")
+streamlit.text("Kale,Spinach")
+streamlit.text("Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
+
